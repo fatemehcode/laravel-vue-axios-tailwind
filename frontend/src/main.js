392f98210b91c5364a413d68e4c60f3216029f64
Fatemeh Yeganeh <fatemeh.code@gmail.com>
2022-12-08 20:46:46 +0100

    laravel-vue-axios-tailwind

---
+++ frontend/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './index.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8000/api/';
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
